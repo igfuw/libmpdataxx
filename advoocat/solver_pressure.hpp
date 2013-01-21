715c1f4ed57a689136bf9d82b4ba56dd58d8972e
trontrytel <ajaruga@igf.fuw.edu.pl>
2013-01-21 02:15:43 +0100

    wip on bombel

--- advoocat/solver_pressure.hpp
+++ advoocat/solver_pressure.hpp
@@ -18,13 +18,15 @@ class pressure_solver : public homo_solver
   using real_t = typename homo_solver::real_t;
 
   real_t dt;
+  rng_t im, jm;
   virtual void forcings(real_t dt) = 0;
 
   void extrp_velocity(int e, int t) //extrapolate in time to t+1/2
   {                  // psi[n-1] will not be used anymore, and it will be intentionally overwritten!
-    auto tmp = this->psi[e][this->n[e] + (t!=0?-1:0)];
-    tmp /= -2;
-    tmp += 3./2 * this->psi[e][this->n[e]]; 
+
+      auto tmp = this->psi[e][this->n[e] -1];
+      tmp /= -2;
+      tmp += 3./2 * this->psi[e][this->n[e]]; 
   }
 
   void intrp_courant(real_t dt) //interpolate in space to courant field
@@ -34,21 +36,23 @@ class pressure_solver : public homo_solver
 
     this->xchng(u, 1);  // filling halos for velocity filed
     this->xchng(w, 1);  // psi[n-1] was overwriten for that by extrp_velocity
-                        // so in xchng previous time level has to be used
 
     auto tmp_u = this->psi[u][this->n[u]-1];
     auto tmp_w = this->psi[w][this->n[w]-1];
   
     //TODO - don't assume dx=1
     real_t dx = 1;
-    this->courant(u)(i+h,j  ) = dt / dx * .5 * (tmp_u(i^h,j  )+tmp_u((i^h)+1, j     ));
-    this->courant(w)(i  ,j+h) = dt / dx * .5 * (tmp_w(i  ,j^h)+tmp_w( i     ,(j^h)+1));
+
+    this->courant(u)(im+h,j  ) = dt / dx * .5 * (tmp_u(im,j  )+tmp_u(im+1, j     ));
+    this->courant(w)(i  ,jm+h) = dt / dx * .5 * (tmp_w(i  ,jm)+tmp_w( i     ,jm+1));
   }
 
   public:
   // ctor
   pressure_solver(int nx, int ny, real_t dt) :
-    homo_solver(nx, ny), dt(dt)
+    homo_solver(nx, ny), dt(dt),
+    im(this->i.first() - 1, this->i.last()),
+    jm(this->j.first() - 1, this->j.last())
   {}
 
   void solve(int nt)
