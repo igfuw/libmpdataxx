1be04257b12ab482b9dc5ed0ced77b3ab3482b47
slayoo <slayoo@igf.fuw.edu.pl>
2013-02-11 15:33:03 +0100

    foxong indentation in cyclic_2d

--- advoocat/bcond/cyclic_2d.hpp
+++ advoocat/bcond/cyclic_2d.hpp
@@ -13,26 +13,26 @@ namespace advoocat
 {
   namespace bcond
   {
-template<int d, typename real_t = float>
-class cyclic_2d : cyclic_common<2, real_t>
-{
-  using parent_t = cyclic_common<2, real_t>;
-  using arr_2d_t = typename parent_t::arr_t;
+    template<int d, typename real_t = float>
+    class cyclic_2d : cyclic_common<2, real_t>
+    {
+      using parent_t = cyclic_common<2, real_t>;
+      using arr_2d_t = typename parent_t::arr_t;
 
-  public:
+      public:
 
-  // ctor
-  cyclic_2d(const rng_t &i, int halo) :
-    parent_t(i, halo)
-  {} 
+      // ctor
+      cyclic_2d(const rng_t &i, int halo) :
+	parent_t(i, halo)
+      {} 
 
-  // method invoked by the solver
-  void fill_halos(const arr_2d_t &a, const rng_t &j)
-  {
-    using namespace idxperm;
-    a(pi<d>(this->left_halo, j)) = a(pi<d>(this->rght_edge, j));     
-    a(pi<d>(this->rght_halo, j)) = a(pi<d>(this->left_edge, j));     
-  }
-};
+      // method invoked by the solver
+      void fill_halos(const arr_2d_t &a, const rng_t &j)
+      {
+	using namespace idxperm;
+	a(pi<d>(this->left_halo, j)) = a(pi<d>(this->rght_edge, j));     
+	a(pi<d>(this->rght_halo, j)) = a(pi<d>(this->left_edge, j));     
+      }
+    };
   }; // namespace bcond
 }; // namespace advoocat
