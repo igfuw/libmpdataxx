56952228663121ff079fb423ce3cdb50eb40d302
slayoo <slayoo@igf.fuw.edu.pl>
2013-02-13 22:59:29 +0100

    adding missing files

---
+++ advoocat/concurr/detail/concurr_common.hpp
@@ -0,0 +1,189 @@
+/** @file
+ * @copyright University of Warsaw
+ * @section LICENSE
+ * GPLv3+ (see the COPYING file or http://www.gnu.org/licenses/)
+ */
+
+#pragma once
+
+#include <boost/ptr_container/ptr_vector.hpp>
+#include "../../blitz.hpp"
+
+#include "../../bcond/cyclic_1d.hpp"
+#include "../../bcond/cyclic_2d.hpp"
+#include "../../bcond/cyclic_3d.hpp"
+// TODO: split into 1D, 2D and 3D files?
+
+#include "sharedmem.hpp"
+
+namespace advoocat
+{
+  namespace concurr
+  {
+    namespace detail
+    {
+      template <
+        class solver_t, 
+        bcond::bcond_e bcx,
+        bcond::bcond_e bcy,
+        bcond::bcond_e bcz
+      >
+      class concurr_common
+      {
+        // helper method to define subdomain ranges
+	int min(int span, int rank, int size) 
+	{ 
+	  return rank * span / size; 
+	}
+
+	int max(int span, int rank, int size) 
+	{ 
+	  return min(span, rank + 1, size) - 1; 
+	}
+
+	protected:
+
+        // (cannot be nested due to tempaltes)
+        typedef sharedmem<
+          typename solver_t::real_t,
+          solver_t::n_dims,
+          solver_t::n_eqs
+        > mem_t;
+
+	// member fields
+	boost::ptr_vector<solver_t> algos; 
+        std::unique_ptr<mem_t> mem;
+
+	public:
+
+        typedef typename solver_t::real_t real_t;
+
+	// 1D ctor
+	concurr_common(
+	  const int s0, 
+	  const typename solver_t::params_t &params,
+          mem_t *mem_p,
+	  const int n0
+	)
+	{
+          mem.reset(mem_p);
+	  solver_t::alloc(mem.get(), s0);
+
+          std::unique_ptr<bcond::bcond_t<typename solver_t::real_t>> bxl, bxr; // TODO: solver_t::bc_p
+
+          if (bcx == bcond::cyclic)  // TODO: make a function taht does it
+          {
+            bxl.reset(new bcond::cyclic_left_1d<typename solver_t::real_t>(rng_t(0, s0-1), solver_t::halo));
+            bxr.reset(new bcond::cyclic_rght_1d<typename solver_t::real_t>(rng_t(0, s0-1), solver_t::halo));
+          }
+          else assert(false);
+
+	  for (int i0 = 0; i0 < n0; ++i0) 
+          {
+            const rng_t i(min(s0, i0, n0), max(s0, i0, n0)); 
+	    algos.push_back(new solver_t(mem.get(), bxl, bxr, i, params));
+          }
+	}
+
+	// 2D ctor
+	concurr_common(
+	  const int s0, const int s1, 
+	  const typename solver_t::params_t &params,
+          mem_t *mem_p,
+	  const int n0, const int n1
+	)
+	{
+          mem.reset(mem_p);
+          solver_t::alloc(mem.get(), s0, s1);
+
+	  std::unique_ptr<bcond::bcond_t<typename solver_t::real_t>> bxl, bxr, byl, byr;
+
+	  if (bcx == bcond::cyclic)  // TODO: make a function taht does it
+	  {
+	    bxl.reset(new bcond::cyclic_left_2d<0, typename solver_t::real_t>(rng_t(0, s0-1), solver_t::halo));
+	    bxr.reset(new bcond::cyclic_rght_2d<0, typename solver_t::real_t>(rng_t(0, s0-1), solver_t::halo));
+	  } 
+	  else assert(false);
+
+	  if (bcy == bcond::cyclic)  // TODO: make a function taht does it
+	  {
+	    byl.reset(new bcond::cyclic_left_2d<1, typename solver_t::real_t>(rng_t(0, s1-1), solver_t::halo));
+	    byr.reset(new bcond::cyclic_rght_2d<1, typename solver_t::real_t>(rng_t(0, s1-1), solver_t::halo));
+	  }
+	  else assert(false);
+
+          for (int i0 = 0; i0 < n0; ++i0) 
+          {
+            for (int i1 = 0; i1 < n1; ++i1) 
+            {
+              const rng_t 
+                i( min(s0, i0, n0), max(s0, i0, n0) ),
+                j( min(s1, i1, n1), max(s1, i1, n1) );
+              algos.push_back(new solver_t(mem.get(), bxl, bxr, byl, byr, i, j, params));
+            }
+          }
+	}
+
+	// 3D ctor
+	concurr_common(
+	  const int s0, const int s1, const int s2, 
+	  const typename solver_t::params_t &params,
+          mem_t *mem_p,
+	  const int n0, const int n1, const int n2
+	)
+	{
+          mem.reset(mem_p);
+	  solver_t::alloc(mem.get(), s0, s1, s2);
+
+	  std::unique_ptr<bcond::bcond_t<typename solver_t::real_t>> bxl, bxr, byl, byr, bzl, bzr;
+
+	  if (bcx == bcond::cyclic) // TODO: make a function that does it
+	  {
+	    bxl.reset(new bcond::cyclic_left_3d<0, typename solver_t::real_t>(rng_t(0, s0-1), solver_t::halo));
+	    bxr.reset(new bcond::cyclic_rght_3d<0, typename solver_t::real_t>(rng_t(0, s0-1), solver_t::halo));
+	  }
+	  else assert(false);
+
+	  if (bcy == bcond::cyclic) // TODO: make a function taht does it
+	  {
+	    byl.reset(new bcond::cyclic_left_3d<1, typename solver_t::real_t>(rng_t(0, s1-1), solver_t::halo));
+	    byr.reset(new bcond::cyclic_rght_3d<1, typename solver_t::real_t>(rng_t(0, s1-1), solver_t::halo));
+	  }
+	  else assert(false);
+
+	  if (bcz == bcond::cyclic)  // TODO: make a function taht does it
+	  {
+	    bzl.reset(new bcond::cyclic_left_3d<2, typename solver_t::real_t>(rng_t(0, s2-1), solver_t::halo));
+	    bzr.reset(new bcond::cyclic_rght_3d<2, typename solver_t::real_t>(rng_t(0, s2-1), solver_t::halo));
+	  }
+	  else assert(false);
+
+	  for (int i0 = 0; i0 < n0; ++i0) 
+          {
+	    for (int i1 = 0; i1 < n1; ++i1) 
+            {
+	      for (int i2 = 0; i2 < n2; ++i2) 
+              {
+                rng_t
+                  i( min(s0, i0, n0), max(s0, i0, n0) ),
+                  j( min(s1, i1, n1), max(s1, i1, n1) ),
+                  k( min(s2, i2, n2), max(s2, i2, n2) );
+		algos.push_back(new solver_t(mem.get(), bxl, bxr, byl, byr, bzl, bzr, i, j, k, params));
+              }
+            }
+          }
+        }
+
+	typename solver_t::arr_t state(int e = 0)
+	{
+	  return mem->state(e);
+	}
+
+	typename solver_t::arr_t courant(int d = 0)
+	{
+	  return mem->courant(d);
+	}
+      };
+    }; // namespace detail
+  }; // namespace concurr
+}; // namespace advoocat
---
+++ advoocat/concurr/detail/sharedmem.hpp
@@ -0,0 +1,130 @@
+/** @file
+ * @copyright University of Warsaw
+ * @section LICENSE
+ * GPLv3+ (see the COPYING file or http://www.gnu.org/licenses/)
+ */
+
+#pragma once
+
+#include <unordered_map>
+#include <boost/ptr_container/ptr_vector.hpp>
+
+#include "../../blitz.hpp"
+
+namespace advoocat
+{
+  namespace concurr
+  {
+    namespace detail
+    {
+      template <
+	typename real_t,
+	int n_dims,
+	int n_eqs
+      >  
+      class sharedmem_common
+      {
+	static_assert(n_eqs > 0, "n_eqs <= 0");
+	static_assert(n_dims > 0, "n_dims <= 0");
+
+        protected:
+
+	int span[n_dims];
+
+	public:
+
+	std::vector<int> n;
+
+	arrvec_t<blitz::Array<real_t, n_dims>> C, psi[n_eqs];
+
+	std::unordered_map<
+	  std::string, // intended for addressing with string(__FILE__)
+	  boost::ptr_vector<arrvec_t<blitz::Array<real_t, n_dims>>>
+	> tmp; 
+
+	// accessor method for the Courant number field
+	blitz::Array<real_t, n_dims> courant(int d = 0)  
+	{   
+	  return C[d]; // TODO: what about halo? (in y)
+	}   
+
+	// ctor
+	sharedmem_common()
+	  : n(n_eqs, 0)
+	{}
+
+        virtual void barrier()
+        {
+          assert(false && "sharedmem_common::barrier() called!");
+        }
+      };
+
+      template<typename real_t, int n_dims, int n_eqs>
+      class sharedmem
+      {};
+
+      template<typename real_t, int n_eqs>
+      class sharedmem<real_t, 1, n_eqs> : public sharedmem_common<real_t, 1, n_eqs>
+      {
+	public:
+
+	blitz::Array<real_t, 1> state(int e)
+	{
+	  return this->psi[e][ this->n[e] ](
+	    rng_t(0, this->span[0]-1)
+	  ).reindex({0});
+	}
+
+	// ctor
+	sharedmem(int s0)
+	{
+	  this->span[0] = s0; 
+	}
+      };
+
+      template<typename real_t, int n_eqs>
+      class sharedmem<real_t, 2, n_eqs> : public sharedmem_common<real_t, 2, n_eqs>
+      {
+	public:
+
+	blitz::Array<real_t, 2> state(int e)
+	{
+	  return this->psi[e][ this->n[e] ](idx_t<2>({
+	    rng_t(0, this->span[0]-1),
+	    rng_t(0, this->span[1]-1)
+	  })).reindex({0, 0});
+	}
+
+	// ctor
+	sharedmem(int s0, int s1)
+	{
+	  this->span[0] = s0; 
+	  this->span[1] = s1; 
+	}
+      };
+
+      template<typename real_t, int n_eqs>
+      class sharedmem<real_t, 3, n_eqs> : public sharedmem_common<real_t, 2, n_eqs>
+      {
+	public:
+
+	blitz::Array<real_t, 3> state(int e)
+	{
+	  return this->psi[e][ this->n[e] ](idx_t<3>({
+	    rng_t(0, this->span[0]-1),
+	    rng_t(0, this->span[1]-1),
+	    rng_t(0, this->span[2]-1)
+	  })).reindex({0, 0, 0});
+	}
+
+	// ctor
+	sharedmem(int s0, int s1, int s2)
+	{
+	  this->span[0] = s0; 
+	  this->span[1] = s1; 
+	  this->span[2] = s2; 
+	}
+      };
+    }; // namespace detail
+  }; // namespace concurr
+}; // namespace advoocat
