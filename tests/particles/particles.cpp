696e55821dd8963200c50e17552073afd001cf0e
slayoo <slayoo@igf.fuw.edu.pl>
2013-05-20 19:58:31 +0200

    adding some asserts to test_particles

--- tests/particles/particles.cpp
+++ tests/particles/particles.cpp
@@ -7,6 +7,14 @@
 template <typename real_t, int thrust_device_system>
 void particles<real_t, thrust_device_system>::func()
 {
+#if (THRUST_DEVICE_SYSTEM == THRUST_DEVICE_SYSTEM_OMP) 
+  assert(device_system_macro == openmp);
+#elif (THRUST_DEVICE_SYSTEM == THRUST_DEVICE_SYSTEM_CUDA) 
+  assert(device_system_macro == cuda);
+#elif (THRUST_DEVICE_SYSTEM == THRUST_DEVICE_SYSTEM_CPP) 
+  assert(device_system_macro == cpp);
+#endif
+
   std::cerr << "CUDA/OpenMP/CPP: " << thrust_device_system << std::endl;
   thrust::device_vector<real_t> vec(1024*1024);
 }
