317d13007b36e9a67d90f1674c1b6ba878af5ba4
slayoo <slayoo@igf.fuw.edu.pl>
2013-05-20 19:22:58 +0200

    test_particles... test

--- tests/particles/particles.cpp
+++ tests/particles/particles.cpp
@@ -2,10 +2,13 @@
 
 #include "particles.hpp"
 
+#include <thrust/device_vector.h>
+
 template <typename real_t, int thrust_device_system>
 void particles<real_t, thrust_device_system>::func()
 {
   std::cerr << "CUDA/OpenMP/CPP: " << thrust_device_system << std::endl;
+  thrust::device_vector<real_t> vec(1024*1024);
 }
 
 template class particles<float, device_system_macro>;
