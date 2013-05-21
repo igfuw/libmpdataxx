d7d34adda9e106d05b545e8457b10ff10d15283d
slayoo <slayoo@igf.fuw.edu.pl>
2013-05-21 19:58:29 +0200

    removing test particles (moved to libcloudph)

--- tests/particles/particles_cuda.cu
+++
@@ -1,7 +0,0 @@
-// including it first not to require pthread option to nvcc
-//#include <blitz/array.h>
-
-#define THRUST_DEVICE_SYSTEM THRUST_DEVICE_SYSTEM_CUDA
-#define libcloudphxx_particles_device cuda
-#define libcloudphxx_particles_real_t float
-#include <libcloudph++/lgrngn/particles.ipp>
