06dba20e887ab63ff383db6362d03a547c7d665c
slayoo <slayoo@igf.fuw.edu.pl>
2013-06-04 11:52:10 +0200

    addint still apparently not functional toa option in 1d (+ new test)

--- libmpdata++/formulae/mpdata/formulae_mpdata_1d.hpp
+++ libmpdata++/formulae/mpdata/formulae_mpdata_1d.hpp
@@ -42,15 +42,53 @@ namespace libmpdataxx
         )
       ) 
 
+      // 3rd order term (first term from eq. (36) from @copybrief Smolarkiewicz_and_Margolin_1998 (with G=1))
+      template<opts_t opts, class arr_1d_t>
+      inline auto f_3rd_xx(
+        const arr_1d_t &psi,
+        const arr_1d_t &C,
+        const rng_t &i
+      ) return_macro(,
+	(3 * C(i+h) * abs(C(i+h)) - 2 * pow(C(i+h), 3) - C(i+h)) 
+        / (typename arr_1d_t::T_numtype(3)) // TODO: chec if this cast is needed
+	* where(
+          // if
+          opt_set(opts, sss),
+          // then
+          frac(
+	    psi(i+2) - psi(i+1) - psi(i) + psi(i-1)
+	    ,// 
+	    psi(i+2) + psi(i+1) + psi(i) + psi(i-1)
+	  ),
+          // else
+          frac(
+	    abs(psi(i+2)) - abs(psi(i+1)) - abs(psi(i)) + abs(psi(i-1))
+	    ,// 
+	    abs(psi(i+2)) + abs(psi(i+1)) + abs(psi(i)) + abs(psi(i-1))
+	  )
+        )
+      )
+
+      //
       template<opts_t opts, class arr_1d_t>
       inline auto antidiff(
         const arr_1d_t &psi, 
         const rng_t &i, 
         const arr_1d_t &C
       ) return_macro(,
+        // second-order terms
         abs(C(i+h)) 
         * (1 - abs(C(i+h))) 
         * A<opts>(psi, i) 
+        // third-order terms
+        + where(
+          // if
+          opt_set(opts, toa),
+          // then 
+          f_3rd_xx<opts>(psi, C, i),
+          // else
+          0
+        )
       ) 
     }; // namespace mpdata
   }; // namespace formulae
---
+++ tests/mpdata_1d_opt_toa/test_mpdata_1d_opt_toa.cpp
@@ -0,0 +1,66 @@
+/** 
+ * @file
+ * @copyright University of Warsaw
+ * @section LICENSE
+ * GPLv3+ (see the COPYING file or http://www.gnu.org/licenses/)
+ * @brief example showing how to use the toa option of mpdata 
+ *   (third-rder accuracy)
+ *
+ * \include "mpdata_1d_opt_sss/test_mpdata_1d_opt_toa.cpp"
+ * \image html "../../tests/mpdata_1d_opt_sss/figure_iters=3.svg" TODO
+ */
+
+#include <libmpdata++/solvers/mpdata_fct_1d.hpp>
+#include <libmpdata++/bcond/bcond.hpp>
+#include <libmpdata++/concurr/threads.hpp>
+#include <libmpdata++/output/gnuplot.hpp>
+
+using namespace libmpdataxx;
+
+using real_t = float;
+int n = 500, nt = 1600;
+
+template <class T>
+void setup(T &solver, int n) 
+{
+  blitz::firstIndex i;
+  int width = 50, center = 100;
+  solver.state(0) = where(i <= center-width/2 || i >= center+width/2,  2, 4); 
+  solver.courant() = .5; 
+}
+
+template <class T>
+void setopts(T &p, const int nt, const std::string &fname)
+{
+  p.outfreq = nt; // displays initial condition and the final state
+  p.gnuplot_output = fname + ".svg";    
+  p.outvars = {
+    {0, {.name = "psi", .unit = "1"}},
+  };
+  p.gnuplot_command = "plot";
+  p.gnuplot_with = "histeps";
+  p.gnuplot_yrange = "[1.75:4.25]";
+}
+
+template <class solver_t, class vec_t>
+void add_solver(vec_t &slvs, const std::string &fname)
+{
+  using output_t = output::gnuplot<solver_t>;
+  typename output_t::params_t p;
+  setopts(p, nt, fname);
+  slvs.push_back(new concurr::threads<output_t, bcond::cyclic>(n, p));
+  setup(slvs.back(), n);
+}
+
+int main() 
+{
+  const int n_dims = 1;
+  boost::ptr_vector<concurr::any<real_t, n_dims>> slvs;
+
+  const int n_eqs = 2;
+  add_solver<solvers::mpdata_1d<real_t, 2, n_eqs>>(slvs, "mpdata_iters=2");
+  add_solver<solvers::mpdata_1d<real_t, 2, n_eqs, formulae::mpdata::toa>>(slvs, "mpdata_iters=2_toa");
+  add_solver<solvers::mpdata_fct_1d<real_t, 2, n_eqs, formulae::mpdata::toa>>(slvs, "mpdata_fct_iters=2_toa");
+
+  for (auto &slv : slvs) slv.advance(nt);
+}
