2878c82936ed3aed79a005372e6393b2f24eb572
slayoo <slayoo@igf.fuw.edu.pl>
2013-07-27 19:26:46 +0200

    fixing thread-name monitor for short process names (e.g. icicle)

--- libmpdata++/solvers/adv/detail/monitor.hpp
+++ libmpdata++/solvers/adv/detail/monitor.hpp
@@ -9,9 +9,14 @@ void monitor(float frac)
 {
   char name[16];
   prctl(PR_GET_NAME, name, 0, 0, 0);
+  static int len = strlen(name);
   // ...     1  0  0  % \0
   // ... 10 11 12 13 14 15
-  int len = sprintf(&name[10], " %3d%%", int(frac * 100));
+  sprintf(
+    &name[std::min(len, 10)], // taking care of short thread-names
+    " %3d%%", 
+    int(frac * 100)
+  );
   prctl(PR_SET_NAME, name, 0, 0, 0);
 }
 #endif
