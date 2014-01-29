a8bd3aa277777b18ec52c093377471899b413d84
trontrytel <ajaruga@igf.fuw.edu.pl>
2014-01-29 22:38:32 +0100

    wip on bug found in totating_cone

--- tests/rotating_cone/test_rotating_cone.cpp
+++ tests/rotating_cone/test_rotating_cone.cpp
@@ -18,24 +18,29 @@ using boost::math::constants::pi;
 #include <libmpdata++/output/gnuplot.hpp>
 using namespace libmpdataxx;
 
-enum {x, y};
-struct ct_params_t : ct_params_default_t
+template <int opts_arg>
+void test(const std::string filename)
 {
-  using real_t = float;
-  enum { n_dims = 2 };
-  enum { n_eqs = 1 };
-  enum { opts = formulae::opts::iga | formulae::opts::tot | formulae::opts::fct };
-};
+  enum {x, y};
+  struct ct_params_t : ct_params_default_t
+  {
+    using real_t = float;
+    enum { n_dims = 2 };
+    enum { n_eqs = 1 };
+    enum { opts = opts_arg };
+    //  enum { opts = formulae::opts::iga | formulae::opts::tot | formulae::opts::fct };
+  };
 
-ct_params_t::real_t 
-  dt = .1,
-  dx = 1,
-  dy = 1,
-  omega = -.1,
-  h = 4., // TODO: other name!
-  h0 = 100.; // change it to 1 to see scary things!
+  typename ct_params_t::real_t 
+    dt = .1,
+    dx = 1,
+    dy = 1,
+    omega = .1,
+    h = 4., // TODO: other name!
+    h0 = 0;
+    //  h0 = 100.; // change it to 1 to see scary things!
 
-// Anderson-Fattachi?
+/// @brief settings from @copybrief Anderson_and_Fattahi_1974
 //    dt = 10 * pi<real_t>(),
 //    omega = -.001,// / (2 * pi<real_t>()),
 //    r = 4. * dx,
@@ -43,14 +48,10 @@ ct_params_t::real_t
 //    x0 = 21. * dx,
 //    y0 = 15. * dy;
 
-/// @brief settings from @copybrief Anderson_and_Fattahi_1974
-
-int main() 
-{
   int nt = 628 * 6;
 
-  using solver_t = output::gnuplot<solvers::mpdata<ct_params_t>>;
-  solver_t::rt_params_t p;
+  using sim_t = output::gnuplot<solvers::mpdata<ct_params_t>>;
+  typename sim_t::rt_params_t p;
 
   // pre instantiation
   p.n_iters = 2;
@@ -60,7 +61,7 @@ int main()
   p.outvars[0].name = "psi";
   {
     std::ostringstream tmp;
-    tmp << "figure_iters=" << p.n_iters << "_%s_%d.svg";
+    tmp << filename << "_%s_%d.svg";
     p.gnuplot_output = tmp.str();    
   }
   p.gnuplot_view = "map";
@@ -83,11 +84,11 @@ int main()
   p.gnuplot_term = "svg";
 
   // instantiation
-  concurr::threads<solver_t, bcond::cyclic, bcond::cyclic> slv(p); 
+  concurr::threads<sim_t, bcond::cyclic, bcond::cyclic> run(p); 
 
   // post instantiation
   {
-    ct_params_t::real_t
+    typename ct_params_t::real_t
       r = 15. * dx,
       x0 = 75 * dx,
       y0 = 50 * dy,
@@ -98,18 +99,41 @@ int main()
     blitz::secondIndex j;
 
     // cone shape
-    decltype(slv.advectee()) tmp(slv.advectee().extent());
+    decltype(run.advectee()) tmp(run.advectee().extent());
     tmp = blitz::pow((i+.5) * dx - x0, 2) + blitz::pow((j+.5) * dy - y0, 2);
-    slv.advectee() = h0 + where(tmp - pow(r, 2) <= 0, h * blitz::sqr(1 - tmp / pow(r, 2)), 0.);
+    run.advectee() = h0 + where(tmp - pow(r, 2) <= 0, h * blitz::sqr(1 - tmp / pow(r, 2)), 0.);
 
     // constant angular velocity rotational field
-    slv.advector(x) = -omega * ((j+.5) * dy - yc) * dt / dx;
-    slv.advector(y) =  omega * ((i+.5) * dx - xc) * dt / dy;
+    run.advector(x) = -omega * ((j+.5) * dy - yc) * dt / dx;
+    run.advector(y) =  omega * ((i+.5) * dx - xc) * dt / dy;
     // TODO: an assert confirming that the above did what it should have done
     //       (in context of the advector()'s use of blitz::Array::reindex())
   }
 
   // time stepping
-  slv.advance(nt);
+  run.advance(nt);
 }
 
+int main()
+{
+  {
+    enum { opts = 0 };
+    test<opts>("basic");
+  }
+  {
+    enum { opts = formulae::opts::fct };
+    test<opts>("fct");
+  }
+  {
+    enum { opts = formulae::opts::iga | formulae::opts::tot};
+    test<opts>("iga_tot");
+  }
+  {
+    enum { opts = formulae::opts::iga | formulae::opts::fct};
+    test<opts>("iga_fct");
+  }
+  {
+    enum { opts = formulae::opts::iga | formulae::opts::tot | formulae::opts::fct};
+    test<opts>("iga_fct_tot");
+  }
+}
