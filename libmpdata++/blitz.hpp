64bbaedab50d9d6eb3fe24bd393518f55a53788b
slayoo <slayoo@igf.fuw.edu.pl>
2014-02-26 20:14:16 +0100

    adding a workaround for missing Blitz include for Clang to work in Debug mode

--- libmpdata++/blitz.hpp
+++ libmpdata++/blitz.hpp
@@ -15,9 +15,12 @@
 #if defined(BZ_THREADSAFE)
 #  error libmpdata++ uses blitz::neverDeleteData, please unset BZ_THREADSAFE
 #endif
+
 // force use of #pragma ivdep even if Blitz thinks the compiler does not support it
 // (as of gcc 20140212, it gives an ICE: http://gcc.gnu.org/bugzilla/show_bug.cgi?id=60198)
 //#define BZ_USE_ALIGNMENT_PRAGMAS  
+
+#include <blitz/tv2fastiter.h> // needed by Clang to work in Debug mode
 #include <blitz/array.h>
 
   
