9908b3b2ec2d054a16c4fee9d67cfbb8bc110e50
Dorota Jarecka <djarecka@gmail.com>
2014-04-08 22:05:49 -0600

    adding calling the plot2D_shallow_water_2d.py

--- tests/shallow_water/spreading_drop_2d.cpp
+++ tests/shallow_water/spreading_drop_2d.cpp
@@ -110,6 +110,6 @@ int main()
   test<formulae::opts::fct | formulae::opts::iga>("spreading_drop_2d_fct+iga.out");
   //test<formulae::opts::fct | formulae::opts::abs>("spreading_drop_2d_fct+abs.out");
   system("python ../../../tests/shallow_water/papierplot_shallow_water_2d.py fct+iga");
-  //system("python");
+  system("python ../../../tests/shallow_water/plot2D_shallow_water_2d.py");
 }
 
