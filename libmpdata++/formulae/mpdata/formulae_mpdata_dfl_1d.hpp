9af83ff4c2877f1a48ae44388944ff3ce344b08d
trontrytel <ajaruga@igf.fuw.edu.pl>
2014-06-05 12:53:09 +0200

    divergent flow correction in 1D for abs and iga options

--- libmpdata++/formulae/mpdata/formulae_mpdata_dfl_1d.hpp
+++ libmpdata++/formulae/mpdata/formulae_mpdata_dfl_1d.hpp
@@ -18,6 +18,7 @@ namespace libmpdataxx
       //divergent flow correction see eq. (30) from @copybrief Smolarkiewicz_and_Margolin_1998)
       template<opts_t opts, class arr_1d_t>
       inline auto DFL(
+        const arr_1d_t &psi,
         const arr_1d_t &GC,
         const arr_1d_t &G,
         const rng_t &i,
@@ -29,22 +30,35 @@ namespace libmpdataxx
 
       template<opts_t opts, class arr_1d_t>
       inline auto DFL(
+        const arr_1d_t &psi,    //to have the same arguments as in iga option
         const arr_1d_t &GC,
         const arr_1d_t &G,
-        const rng_t &i,
-        typename std::enable_if<!opts::isset(opts, opts::tot) && opts::isset(opts, opts::dfl)>::type* = 0 
+        const rng_t &i,         //has not been derived for tot yet
+        typename std::enable_if<!opts::isset(opts, opts::tot) && opts::isset(opts, opts::dfl) && !opts::isset(opts, opts::iga)>::type* = 0 
       ) return_macro(,
-        - GC(i+h) / (
-          formulae::G<opts>(G, i+1) 
-          + 
-          formulae::G<opts>(G, i  )
-        ) / 2 * (
-          GC((i+1)+h) 
-          - 
-          GC(i-h)
-        )
+        - 0.5 * GC(i+h) 
+        / 
+        (formulae::G<opts>(G, i+1) + formulae::G<opts>(G, i)) 
+        * 
+        (GC((i+1)+h) - GC(i-h))
       )
-      // TODO: dfl && tot version of the above?
+
+      template<opts_t opts, class arr_1d_t>
+      inline auto DFL(
+        const arr_1d_t &psi,
+        const arr_1d_t &GC,
+        const arr_1d_t &G,
+        const rng_t &i,         //has not been derived for tot yet
+        typename std::enable_if<!opts::isset(opts, opts::tot) && opts::isset(opts, opts::dfl) && opts::isset(opts, opts::iga)>::type* = 0 
+      ) return_macro(,
+        - 0.5 * GC(i+h) 
+        / 
+        (formulae::G<opts>(G, i+1) + formulae::G<opts>(G, i)) 
+        * 
+        (GC((i+1)+h) - GC(i-h))
+        *
+        0.5 *  (psi(i+1) + psi(i)) //to be compatible with iga formulation
+      )
       
     }; // namespace mpdata
   }; // namespace formulae
