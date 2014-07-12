37ecda6f781c9b24b8cf742b631c0a09980e6024
Dorota Jarecka <djarecka@gmail.com>
2014-07-12 10:24:14 -0500

    anallytic solution for eliptic dropl

--- tests/shallow_water/analytic_eq.py
+++ tests/shallow_water/analytic_eq.py
@@ -1,6 +1,7 @@
 import numpy as np
 import math
 from scipy.optimize import fsolve
+from scipy.integrate import odeint
 
 #eq. 5.6 (Schar&Smolarkiewicz, 1996) 
 def d1_lambda_eq(x, *time):
@@ -40,3 +41,27 @@ def d2_velocity(lamb, x, y):
 #initial condition
 def d2_initial(x,y):
     return np.where(d2_rad2(x,y)<=1, 1-d2_rad2(x,y), 0)
+
+# for elliptic:
+def d2_el_height(lamb_x, lamb_y, x, y):
+    return np.where(x**2/lamb_x**2 + y**2/lamb_y**2 <= 1.,
+                    1./lamb_x/lamb_y * (1. - x**2/lamb_x**2 - y**2/lamb_y**2), 0.)
+
+#can be used for both - x and y
+def d2_el_velocity(lamb, lamb_t, lamb_other, x, x_other):
+    return np.where(x**2/lamb**2 + x_other**2/lamb_other**2 <= 1.,
+                    x * lamb_t / lamb, 0)
+
+#for derivation TODO! - rewrite
+def deriv(y,t):
+    # return derivatives of [lambda_x, dlambda_x/dt, lambda_y, dlambda_y/dt
+    return np.array([y[1], 2. / y[0]**2 / y[2], y[3], 2. / y[0] / y[2]**2])
+
+
+def d2_el_lamb_lamb_t_evol(time, lamb_x0, lamb_y0):
+    time_lin = np.linspace(0.0,time,101)
+    yinit = np.array([lamb_x0, 0., lamb_y0, 0.]) # initial values (velocity is 0.)
+    print "lamb_x0, lamb_y0", lamb_x0, lamb_y0, time
+    y = odeint(deriv,yinit,time_lin)
+    print "time, lambda_x, dlambda_x/dt, lambda_y, dlambda_y/dt", time_lin[-1], y[-1,:]
+    return y[-1,:]
