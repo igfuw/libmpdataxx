a0be34f24b3320e20ed8155f46fe5e4303947fd9
trontrytel <ajaruga@igf.fuw.edu.pl>
2014-09-22 17:12:25 +0200

    cleanups for article (sim -> slv_out and vip_eps=1e-8)

--- tests/tutorial/7_shallow_water/shallow_water.cpp
+++ tests/tutorial/7_shallow_water/shallow_water.cpp
@@ -107,7 +107,7 @@ void test(const std::string &pfx)
   p.di = .05;
   p.grid_size = { int(16 / p.di) };
   p.g = 1;
-  p.vip_eps = 1e-10; 
+  p.vip_eps = 1e-8; 
 //</listing-2>
 
   // instantiation
