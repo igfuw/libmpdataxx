25b14506b829486c5327614b74c540f30b4b7db9
Maciej Waruszewski <mwarusz@igf.fuw.edu.pl>
2015-02-13 14:20:01 +0100

    update plotting script for revolving_sphere_3d test

--- tests/tutorial/4_revolving_sphere_3d/plot.py
+++ tests/tutorial/4_revolving_sphere_3d/plot.py
@@ -1,6 +1,7 @@
 USAGE = 'Usage: pvpython plot.py timestep*.xmf'
 
 from sys import argv, exit
+from math import sqrt
 
 try:
     from paraview.simple import *
@@ -11,7 +12,7 @@ except ImportError:
 # checking arguments
 if (
     len(argv) != 2 or
-    not argv[1].startswith('timestep') or
+    not argv[1].rpartition('/')[2].startswith('timestep') or
     not argv[1].endswith('.xmf')
    ):
     print USAGE
@@ -23,17 +24,19 @@ reader = OpenDataFile(fname)
 
 # creating the view
 view = CreateRenderView()
-view.Background = [1, 1, 1]  
+view.Background = [1, 1, 1]
 view.CenterAxesVisibility = False
 view.OrientationAxesVisibility = False
-view.CameraViewUp = [-0.26, -0.09, 0.96]
-view.CameraFocalPoint = [61, 36, 32]
-view.CameraViewAngle = 30
-view.CameraPosition = [143, 69, 58]
+view.CameraViewUp = [0, 0, 1]
 
-# creating threshold of values > 0.5
+d = 25 / sqrt(3)
+view.CameraFocalPoint = [50 - d, 50 + d, 50 + d]
+view.CameraViewAngle = 45
+view.CameraPosition = [200, 200, 100]
+
+# creating threshold of values > 1
 th = Threshold()
-th.ThresholdRange = [0.5, 4]
+th.ThresholdRange = [1, 4]
 
 th_dp = GetDisplayProperties(th)
 th_dp.Representation = "Surface With Edges"
@@ -42,49 +45,28 @@ th_dp.EdgeColor = [0, 0, 0]
 
 Show(th)
 
-# domain diagonal
-diag = Line()
-diag.Point1 = [0, 0, 0]
-diag.Point2 = [35, 35, 35]
-
-diag_dp = GetDisplayProperties(diag)
-diag_dp.AmbientColor = [0,0,0]
-diag_dp.Representation = 'Wireframe'
-diag_dp.LineWidth = 2
-Show(diag)
-
-# x axis
-x_axis = Line()
-x_axis.Point1 = [0, 0, 0]
-x_axis.Point2 = [30, 0, 0]
-
-x_axis_dp = GetDisplayProperties(x_axis)
-x_axis_dp.AmbientColor = [0, 0, 0]
-x_axis_dp.Representation = 'Wireframe'
-x_axis_dp.LineWidth = 2
-Show(x_axis)
-
-# y axis
-y_axis = Line()
-y_axis.Point1 = [0, 0, 0]
-y_axis.Point2 = [0, 30, 0]
-
-y_axis_dp = GetDisplayProperties(y_axis)
-y_axis_dp.AmbientColor = [0, 0, 0]
-y_axis_dp.Representation = 'Wireframe'
-y_axis_dp.LineWidth = 2
-Show(y_axis)
+L = 100
 
 # z axis
-z_axis = Line()
-z_axis.Point1 = [0, 0, 0]
-z_axis.Point2 = [0, 0, 30]
+line = Line()
+line.Point1 = [0, 0, 0]
+line.Point2 = [0, 0, L]
+line_dp = GetDisplayProperties(line)
+line_dp.AmbientColor = [0, 0, 0]
+line_dp.Representation = 'Wireframe'
+line_dp.LineWidth = 2
+Show(line)
 
-z_axis_dp = GetDisplayProperties(z_axis)
-z_axis_dp.AmbientColor = [0, 0, 0]
-z_axis_dp.Representation = 'Wireframe'
-z_axis_dp.LineWidth = 2
-Show(z_axis)
+# xy plane
+plane = Plane()
+plane.Origin = [0, 0, 0]
+plane.Point1 = [L, 0, 0]
+plane.Point2 = [0, L, 0]
+plane_dp = GetDisplayProperties(plane)
+plane_dp.AmbientColor = [0, 0, 0]
+plane_dp.Representation = 'Wireframe'
+plane_dp.LineWidth = 2
+Show(plane)
 
 Render()
 
