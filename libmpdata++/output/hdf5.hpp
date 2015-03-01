13e782f49dc45518f41ef20947ed8111a19da583
slayoo <slayoo@igf.fuw.edu.pl>
2015-03-01 23:25:44 +0100

    debug messages for hdf file ctors

--- libmpdata++/output/hdf5.hpp
+++ libmpdata++/output/hdf5.hpp
@@ -61,7 +61,9 @@ namespace libmpdataxx
 
           // creating the coordinates file
           std::string dim_file = outdir + "/coord.h5";
+std::cerr << "creating " << dim_file << std::endl;
           hdfp.reset(new H5::H5File(dim_file, H5F_ACC_TRUNC));
+std::cerr << "... done (" << dim_file << ")." << std::endl;
 
           // creating the dimensions
           // x,y,z
@@ -157,7 +159,9 @@ namespace libmpdataxx
         //count[1] = 1; TODO
 
         // creating the timestep file
+std::cerr << "creating "<< hdf_name() << " file ..." << std::endl;
         hdfp.reset(new H5::H5File(outdir + "/" + hdf_name(), H5F_ACC_TRUNC));
+std::cerr << "... done (" << hdf_name() << ")." << std::endl;
 
         {
 	  for (const auto &v : this->outvars)
