97ae41636eeb558f81ea7aeae41de6f42c133161
slayoo <slayoo@igf.fuw.edu.pl>
2015-08-12 18:55:33 +0200

    reverting leftover change in basic_example.hpp

--- tests/paper_2015_GMD/0_basic_example/basic_example.cpp
+++ tests/paper_2015_GMD/0_basic_example/basic_example.cpp
@@ -23,7 +23,7 @@ int main()
   
   // concurency choice
   using run_t = concurr::serial<
-    slv_out_t, bcond::cyclic, bcond::cyclic
+    slv_out_t, bcond::open, bcond::open
   >;         //left bcond   //right bcond
 
   // run-time parameters
