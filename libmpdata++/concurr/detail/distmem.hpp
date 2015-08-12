9a8adaf408c8efa08dd7f00e34cd027b931147e1
slayoo <slayoo@igf.fuw.edu.pl>
2015-08-12 18:42:11 +0200

    throwing an exception if mpirun detected but USE_MPI=false

--- libmpdata++/concurr/detail/distmem.hpp
+++ libmpdata++/concurr/detail/distmem.hpp
@@ -3,7 +3,10 @@
 #if defined(USE_MPI)
 #  include <boost/mpi/environment.hpp>
 #  include <boost/mpi/communicator.hpp>
+#else
+#  include <cstdlib>
 #endif
+
 
 namespace libmpdataxx
 {
@@ -35,6 +38,21 @@ namespace libmpdataxx
           return mpicom.size(); 
 #else
           return 1;
+#endif
+        }
+
+        // ctor
+        distmem() 
+        {
+#if !defined(USE_MPI)
+          if (
+            // mpich
+            std::getenv("PMI_RANK") != NULL ||
+            // openmpi
+            std::getenv("OMPI_COMM_WORLD_RANK") != NULL ||
+            // lam
+            std::getenv("LAMRANK") != NULL
+          ) throw std::runtime_error("mpirun environment variable detected but libmpdata++ was compiled with MPI disabled");
 #endif
         }
       };
