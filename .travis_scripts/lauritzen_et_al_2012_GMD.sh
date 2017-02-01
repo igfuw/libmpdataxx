483f0e06286ea552be89af1f2b36119663919edd
Maciej Waruszewski <mwarusz@igf.fuw.edu.pl>
2017-02-01 18:11:11 +0100

    temporarily running new tests on travis

---
+++ .travis_scripts/lauritzen_et_al_2012_GMD.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+set -e
+cd tests/lauritzen_et_al_2012_GMD
+mkdir build
+cd build
+# Travis default is not the packaged one
+if [[ $TRAVIS_OS_NAME == 'linux' && $CXX == 'clang++' ]]; then cmake -DCMAKE_CXX_COMPILER=/usr/bin/clang++ ../; fi
+# the one from homebrew
+if [[ $TRAVIS_OS_NAME == 'osx' && $CXX == 'g++' ]]; then cmake -DCMAKE_CXX_COMPILER=g++-4.8 ../; fi
+
+# compiling everything in the Release mode
+cmake -DCMAKE_BUILD_TYPE=Release ../
+VERBOSE=1 $make_j
+
+# running tests in Release mode 
+# "/" intentional! (just to make cat exit with an error code)
+OMP_NUM_THREADS=4 ctest -V || cat Testing/Temporary/LastTest.log /
+cd ../../..
