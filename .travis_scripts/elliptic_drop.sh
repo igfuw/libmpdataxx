912bd2aa738548928606a0551563c13b235862b8
pdziekan <pdziekanp@gmail.com>
2017-05-16 16:15:53 +0200

    travis: use clang++ in elliptic drop only on linux and clang build; is there anr if/else in travis/

--- .travis_scripts/elliptic_drop.sh
+++ .travis_scripts/elliptic_drop.sh
@@ -9,7 +9,11 @@ git clone --depth=1 git://github.com/igfuw/shallow-water-elliptic-drop.git
 cd shallow-water-elliptic-drop/numerical
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=/usr/bin/clang++ #TODO: Debug mode?
+if [[ $TRAVIS_OS_NAME == 'linux' && $CXX == 'clang++' ]]; then 
+  cmake -DCMAKE_CXX_COMPILER=/usr/bin/clang++ -DCMAKE_BUILD_TYPE=Release ../; 
+else
+  cmake -DCMAKE_BUILD_TYPE=Release ../; 
+fi
 make
 #- ./spreading_drop_2d_el 
 # TODO: make test!
