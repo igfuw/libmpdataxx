e2c238fa303b42e440b166e2408df5339558a0ff
pdziekan <pdziekanp@gmail.com>
2017-05-17 17:18:39 +0200

    travis: use updated names of sandbox tests

--- .travis_scripts/sandbox.sh
+++ .travis_scripts/sandbox.sh
@@ -11,7 +11,7 @@ cmake ..
 VERBOSE=1 $make_j
 # running selected sandbox tests in Release mode
 # "/" intentional! (just to make cat exit with an error code)
-OMP_NUM_THREADS=4 make -C mpi_adv test || cat mpi_adv/Testing/Temporary/LastTest.log /
-OMP_NUM_THREADS=4 make -C convergence_2d_3d test || cat convergence_2d_3d/Testing/Temporary/LastTest.log /
-if [[ $TRAVIS_OS_NAME == 'linux' ]]; then OMP_NUM_THREADS=4 make -C convergence_spacetime test || cat convergence_spacetime/Testing/Temporary/LastTest.log /; fi
+OMP_NUM_THREADS=4 make -C 0_mpi_adv test || cat 0_mpi_adv/Testing/Temporary/LastTest.log /
+OMP_NUM_THREADS=4 make -C 3_convergence_2d_3d test || cat 3_convergence_2d_3d/Testing/Temporary/LastTest.log /
+if [[ $TRAVIS_OS_NAME == 'linux' ]]; then OMP_NUM_THREADS=4 make -C 5_convergence_spacetime test || cat 5_convergence_spacetime/Testing/Temporary/LastTest.log /; fi
 cd ../../..
