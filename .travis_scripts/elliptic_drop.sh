1b7160be80ef21c07451e80402eb99cad3fcd48d
pdziekan <pdziekanp@gmail.com>
2017-05-18 10:08:10 +0200

    travis elliptic drop dont point to clang on linux - no need

--- .travis_scripts/elliptic_drop.sh
+++ .travis_scripts/elliptic_drop.sh
@@ -9,11 +9,7 @@ git clone --depth=1 git://github.com/igfuw/shallow-water-elliptic-drop.git
 cd shallow-water-elliptic-drop/numerical
 mkdir build
 cd build
-if [[ $TRAVIS_OS_NAME == 'linux' && $CXX == 'clang++' ]]; then 
-  cmake -DCMAKE_CXX_COMPILER=/usr/bin/clang++ -DCMAKE_BUILD_TYPE=Release ../; 
-else
-  cmake -DCMAKE_BUILD_TYPE=Release ../; 
-fi
+cmake .. -DCMAKE_BUILD_TYPE=Release #TODO: Debug mode?
 make
 #- ./spreading_drop_2d_el 
 # TODO: make test!
