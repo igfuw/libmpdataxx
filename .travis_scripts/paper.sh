68a6d8ab1a6832e61e111723135dfc10261358a4
pdziekan <pdziekanp@gmail.com>
2017-05-19 12:38:54 +0200

    if MPI run all tests in paper and sandbox

--- .travis_scripts/paper.sh
+++ .travis_scripts/paper.sh
@@ -12,10 +12,6 @@ VERBOSE=1 make -C 6_coupled_harmosc
 # "/" intentional! (just to make cat exit with an error code)
 OMP_NUM_THREADS=4 make -C 6_coupled_harmosc test || cat 6_coupled_harmosc/Testing/Temporary/LastTest.log /
 
-# <TEMPORARY>
-if [[ $MPI != 'none'   ]]; then mpirun -np 2 ./0_basic_example/basic_example; fi
-# </TEMPORARY>
-
 # compiling everything in the Release mode
 cmake -DCMAKE_BUILD_TYPE=Release ../
 VERBOSE=1 $make_j
