73318022b6056fe0a229b0b4e42a78eddae868bf
pdziekan <pdziekanp@gmail.com>
2017-06-01 16:23:52 +0200

    nair tests: explicit mpi init

--- tests/nair_jablonowski_2008/stationary/stationary.cpp
+++ tests/nair_jablonowski_2008/stationary/stationary.cpp
@@ -122,6 +122,11 @@ stat_t<> test(const std::string &base_name, const int ny, const T max_cfl)
 
 int main()
 {
+#if defined(USE_MPI)
+  // we will instantiate many solvers, so we have to init mpi manually, 
+  // because solvers will not know should they finalize mpi upon destruction
+  MPI::Init_thread(MPI_THREAD_SERIALIZED);
+#endif
   const bool var_dt = true;
   const T max_cfl = 0.99;
   {
@@ -147,4 +152,8 @@ int main()
     const int opts_iters = 2;
     convergence(test<var_dt, opts, opts_iters>, "nug_iga_tot_fct_i2", max_cfl);
   }
+#if defined(USE_MPI)
+  MPI::Finalize();
+#endif
+
 }
