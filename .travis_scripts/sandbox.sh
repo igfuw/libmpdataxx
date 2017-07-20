9dc49b110f8431e00c9ba146a7d425de662aaf4c
Maciej Waruszewski <mwarusz@igf.fuw.edu.pl>
2017-07-20 18:06:23 +0200

    add tgv_2d test, run it on travis

--- .travis_scripts/sandbox.sh
+++ .travis_scripts/sandbox.sh
@@ -11,6 +11,7 @@ cmake ..
 VERBOSE=1 $make_j
 # running selected sandbox tests in Release mode
 # "/" intentional! (just to make cat exit with an error code)
+OMP_NUM_THREADS=4 ctest -R tgv_2d || cat Testing/Temporary/LastTest.log /
 OMP_NUM_THREADS=4 make -C convergence_2d_3d test || cat convergence_2d_3d/Testing/Temporary/LastTest.log /
 if [[ $TRAVIS_OS_NAME == 'linux' ]]; then OMP_NUM_THREADS=4 make -C convergence_spacetime test || cat convergence_spacetime/Testing/Temporary/LastTest.log /; fi
 cd ../../..
