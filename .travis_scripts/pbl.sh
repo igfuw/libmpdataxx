4966da21bef8baca1479589c1520d3af9b6444a8
Maciej Waruszewski <mwarusz@igf.fuw.edu.pl>
2017-07-21 13:43:05 +0200

    fixes in pbl.sh

--- .travis_scripts/pbl.sh
+++ .travis_scripts/pbl.sh
@@ -5,14 +5,12 @@ mkdir build
 cd build
 # Travis default is not the packaged one
 if [[ $TRAVIS_OS_NAME == 'linux' && $CXX == 'clang++' ]]; then cmake -DCMAKE_CXX_COMPILER=/usr/bin/clang++ ../; fi
-# the one from homebrew
-if [[ $TRAVIS_OS_NAME == 'osx' && $CXX == 'g++' ]]; then cmake -DCMAKE_CXX_COMPILER=g++-4.8 ../; fi
 cmake ..
 # compiling and running pbl_iles on clang
 # "/" intentional! (just to make cat exit with an error code)
-if [[ $CXX == 'clang++' ]]; then VERBOSE=1 make -C pbl_iles; fi
+if [[ $CXX == 'clang++' ]]; then VERBOSE=1 make pbl_iles; fi
 if [[ $CXX == 'clang++' ]]; then OMP_NUM_THREADS=4 ctest -V -R pbl_iles || cat /Testing/Temporary/LastTest.log /; fi
 # smg on gcc
-if [[ $CXX == 'g++' ]]; then VERBOSE=1 make -C pbl_smg; fi
+if [[ $CXX == 'g++' ]]; then VERBOSE=1 make pbl_smg; fi
 if [[ $CXX == 'g++' ]]; then OMP_NUM_THREADS=4 ctest -V -R pbl_smg || cat /Testing/Temporary/LastTest.log /; fi
 cd ../../..
