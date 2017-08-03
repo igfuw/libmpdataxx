1e5f5331f5d6db56ec49b715947edea42649855d
Maciej Waruszewski <mwarusz@igf.fuw.edu.pl>
2017-08-03 15:57:52 +0200

    [travis] try to fix bicycles build

--- .travis_scripts/bicycles.sh
+++ .travis_scripts/bicycles.sh
@@ -11,7 +11,7 @@ if [[ $PY3DEB != '' ]]; then sudo update-alternatives --remove python /usr/bin/p
 if [[ $PY3DEB != '' ]]; then sudo update-alternatives --install /usr/bin/python python /usr/bin/python2 10; fi
 
 # libcloudph++'s dependencies
-if [[ $TRAVIS_OS_NAME == 'linux' ]]; then sudo $apt_get_install libboost-python-dev python-numpy; fi
+#if [[ $TRAVIS_OS_NAME == 'linux' ]]; then sudo $apt_get_install libboost-python-dev python-numpy; fi
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then brew install boost-python; fi
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then git clone --depth=1 git://github.com/thrust/thrust.git; fi
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then sudo ln -s `pwd`/thrust/thrust /usr/local/include/thrust; fi
@@ -39,7 +39,7 @@ sudo make install
 cd ../..
 
 #bicycles
-if [[ $TRAVIS_OS_NAME == 'linux' ]]; then sudo $apt_get_install libboost-program-options-dev; fi
+#if [[ $TRAVIS_OS_NAME == 'linux' ]]; then sudo $apt_get_install libboost-program-options-dev; fi
 git clone --depth=1 git://github.com/igfuw/bicycles.git
 cd bicycles
 mkdir build
