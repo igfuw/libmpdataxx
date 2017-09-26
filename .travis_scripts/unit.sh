94a7d083150de51a0237d6000f9f21b225188af6
Maciej Waruszewski <mwarusz@igf.fuw.edu.pl>
2017-09-26 13:56:54 +0200

    run unit tests in Release mode too on Travis

--- .travis_scripts/unit.sh
+++ .travis_scripts/unit.sh
@@ -7,4 +7,8 @@ cmake -DCMAKE_BUILD_TYPE=Debug ../
 VERBOSE=1 $make_j
 # "/" intentional! (just to make cat exit with an error code)
 OMP_NUM_THREADS=4 make test || cat Testing/Temporary/LastTest.log /
+cmake -DCMAKE_BUILD_TYPE=Release ../
+VERBOSE=1 $make_j
+# "/" intentional! (just to make cat exit with an error code)
+OMP_NUM_THREADS=4 make test || cat Testing/Temporary/LastTest.log /
 cd ../../..
