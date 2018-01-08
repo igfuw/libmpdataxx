896737eb43568404b18f9b530ef96e207e7d2a88
Piotr Dziekan <pdziekan@igf.fuw.edu.pl>
2018-01-08 16:08:21 +0100

    in sandbox tests that need explicit mpi init, change from serialized to multiple threads

--- tests/sandbox/2_tgv/tgv_2d.cpp
+++ tests/sandbox/2_tgv/tgv_2d.cpp
@@ -112,7 +112,7 @@ int main()
 #if defined(USE_MPI)
   // we will instantiate many solvers, so we have to init mpi manually, 
   // because solvers will not know should they finalize mpi upon destruction
-  MPI::Init_thread(MPI_THREAD_SERIALIZED);
+  MPI::Init_thread(MPI_THREAD_MULTIPLE);
 #endif
 
   if (!check<opts::iga, 2>()) throw std::runtime_error("iga");
