bf5b196f824c10f552834e94fdd3f3feae682af7
Piotr Dziekan <pdziekan@igf.fuw.edu.pl>
2018-01-11 16:17:34 +0100

    remote_1d: add remaining fill_halos_...

--- libmpdata++/bcond/remote_1d.hpp
+++ libmpdata++/bcond/remote_1d.hpp
@@ -35,6 +35,15 @@ namespace libmpdataxx
         this->xchng(a, idx_t(idx_ctor_arg_t(this->left_intr_sclr + off)), idx_t(idx_ctor_arg_t(this->left_halo_sclr)));
       }
 
+      void fill_halos_pres(arr_t &a)
+      {
+        fill_halos_sclr(a);
+      }
+      
+      void save_edge_vel(const arr_t &) {}
+
+      void set_edge_pres(arr_t &, int) {}
+
       void fill_halos_vctr_alng(arrvec_t<arr_t> &av, const bool ad = false)
       {
         if(!this->is_cyclic) 
@@ -49,7 +58,51 @@ namespace libmpdataxx
           // cyclic should communicate both ways 
           this->xchng(av[0], idx_t(idx_ctor_arg_t(this->left_intr_vctr + off)), idx_t(idx_ctor_arg_t(this->left_halo_vctr))); 
       }
+      
+      void fill_halos_sgs_div(arr_t &a)
+      {
+        fill_halos_sclr(a);
+      }
+
+      void fill_halos_sgs_vctr(arrvec_t<arr_t> &av, const arr_t &, const int offset = 0)
+      {
+	using namespace idxperm;
+        // the same logic as fill_halos_vctr_alng but have to consider offset ... TODO: find a way to reuse !
+        if(!this->is_cyclic) 
+        {
+          if(halo == 1)
+            this->send(av[0 + offset], idx_t(idx_ctor_arg_t(this->left_intr_vctr + off))); 
+          else
+            // processes fill vectors to the left of their domain
+            this->xchng(av[0 + offset], idx_t(idx_ctor_arg_t(this->left_intr_vctr + off)), idx_t(idx_ctor_arg_t((this->left_halo_vctr^h)^(-1)))); 
+        }
+        else 
+          // cyclic should communicate both ways 
+          this->xchng(av[0 + offset], idx_t(idx_ctor_arg_t(this->left_intr_vctr + off)), idx_t(idx_ctor_arg_t(this->left_halo_vctr))); 
+      }
+      
+      void fill_halos_sgs_tnsr(arrvec_t<arr_t> &av, const arr_t &, const arr_t &, const real_t)
+      {
+        fill_halos_vctr_alng(av);
+      }
+
+      void fill_halos_vctr_nrml(arr_t &a)                 
+      {                                                                         
+        fill_halos_sclr(a);                                                  
+      }  
+      
+      void fill_halos_vctr_alng_cyclic(arrvec_t<arr_t> &av, const bool ad = false)
+      {
+        fill_halos_vctr_alng(av, ad);
+      }
+
+      void fill_halos_vctr_nrml_cyclic(arr_t &a)
+      {
+        fill_halos_vctr_nrml(a);
+      }
     };
+
+
 
     template <typename real_t, int halo, bcond_e knd, drctn_e dir, int n_dims, int dim>
     class bcond<       real_t,     halo,         knd,         dir,     n_dims,     dim,
@@ -75,6 +128,16 @@ namespace libmpdataxx
         this->xchng(a, idx_t(idx_ctor_arg_t(this->rght_intr_sclr + off)), idx_t(idx_ctor_arg_t(this->rght_halo_sclr)));
       }
 
+      void fill_halos_pres(arr_t &a)
+      {
+        fill_halos_sclr(a);
+      }
+      
+      void save_edge_vel(const arr_t &) {}
+
+      void set_edge_pres(arr_t &, int) {}
+
+
       void fill_halos_vctr_alng(arrvec_t<arr_t> &av, const bool ad = false)
       {
         if(!this->is_cyclic) 
@@ -86,6 +149,46 @@ namespace libmpdataxx
         }
         else
           this->xchng(av[0], idx_t(idx_ctor_arg_t(this->rght_intr_vctr + off)), idx_t(idx_ctor_arg_t(this->rght_halo_vctr))); 
+      }
+      
+      void fill_halos_sgs_div(arr_t &a)
+      {
+        fill_halos_sclr(a);
+      }
+
+      void fill_halos_sgs_vctr(arrvec_t<arr_t> &av, const arr_t &, const int offset = 0)
+      {
+	using namespace idxperm;
+        // the same logic as fill_halos_vctr_alng but have to consider offset ... TODO: find a way to reuse !
+        if(!this->is_cyclic) 
+        {
+          if(halo == 1)
+            this->recv(av[0 + offset],  idx_t(idx_ctor_arg_t(this->rght_halo_vctr))); 
+          else
+            this->xchng(av[0 + offset], idx_t(idx_ctor_arg_t(((this->rght_intr_vctr + off)^h)^(-1))), idx_t(idx_ctor_arg_t(this->rght_halo_vctr))); 
+        }
+        else
+          this->xchng(av[0 + offset], idx_t(idx_ctor_arg_t(this->rght_intr_vctr + off)), idx_t(idx_ctor_arg_t(this->rght_halo_vctr))); 
+      }
+      
+      void fill_halos_sgs_tnsr(arrvec_t<arr_t> &av, const arr_t &, const arr_t &, const real_t)
+      {
+        fill_halos_vctr_alng(av);
+      }
+
+      void fill_halos_vctr_nrml(arr_t &a)                 
+      {                                                                         
+        fill_halos_sclr(a);                                                  
+      }  
+      
+      void fill_halos_vctr_alng_cyclic(arrvec_t<arr_t> &av, const bool ad = false)
+      {
+        fill_halos_vctr_alng(av, ad);
+      }
+
+      void fill_halos_vctr_nrml_cyclic(arr_t &a)
+      {
+        fill_halos_vctr_nrml(a);
       }
     };
   } // namespace bcond
