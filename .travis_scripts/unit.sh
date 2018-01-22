8164b427c738860b0d5ac8d19fa8c5a2894a30a7
Piotr Dziekan <pdziekan@igf.fuw.edu.pl>
2018-01-22 15:23:51 +0100

    travis mpi unit tests: var_dt only in Release mode, Debug is too slow

--- .travis_scripts/unit.sh
+++ .travis_scripts/unit.sh
@@ -9,7 +9,7 @@ VERBOSE=1 $make_j
 if [[ $MPI == 'none' ]]; then OMP_NUM_THREADS=4 make test || cat Testing/Temporary/LastTest.log /; fi
 
 # don't run Debug bconds and absorber tests on MPI as it takes too long
-if [[ $MPI != 'none' ]]; then OMP_NUM_THREADS=2 make -C var_dt test || cat var_dt/Testing/Temporary/LastTest.log /; fi
+# don't run var_dt either - takes too long or stalls? it takes ca. 20s on cuda-k-2
 if [[ $MPI != 'none' ]]; then OMP_NUM_THREADS=2 make -C kahan_sum test || cat kahan_sum/Testing/Temporary/LastTest.log /; fi
 if [[ $MPI != 'none' ]]; then OMP_NUM_THREADS=2 make -C cone_bugs test || cat cone_bugs/Testing/Temporary/LastTest.log /; fi
 if [[ $MPI != 'none' ]]; then OMP_NUM_THREADS=2 make -C shallow_water test || cat shallow_water/Testing/Temporary/LastTest.log /; fi
@@ -20,8 +20,10 @@ cmake -DCMAKE_BUILD_TYPE=Release ../
 # on MPI run absorber and bconds in release mode
 if [[ $MPI != 'none' ]]; then VERBOSE=1 make -C absorber; fi
 if [[ $MPI != 'none' ]]; then VERBOSE=1 make -C bconds; fi
+if [[ $MPI != 'none' ]]; then VERBOSE=1 make -C var_dt; fi
 if [[ $MPI != 'none' ]]; then make -C absorber test || cat absorber/Testing/Temporary/LastTest.log /; fi
 if [[ $MPI != 'none' ]]; then OMP_NUM_THREADS=2 make -C bconds test || cat bconds/Testing/Temporary/LastTest.log /; fi
+if [[ $MPI != 'none' ]]; then OMP_NUM_THREADS=2 make -C var_dt test || cat var_dt/Testing/Temporary/LastTest.log /; fi
 
 if [[ $MPI == 'none' ]]; then VERBOSE=1 $make_j; fi
 # excluding test_issue because it (sometimes) fails in Release mode
