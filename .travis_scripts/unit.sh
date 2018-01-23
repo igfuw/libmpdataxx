46c3f305a40baba8f10fdc513c75f04157557f45
Piotr Dziekan <pdziekan@igf.fuw.edu.pl>
2018-01-23 16:41:52 +0100

    travis mpi unit tests: dont run var_dt and bconds cause they r too slow

--- .travis_scripts/unit.sh
+++ .travis_scripts/unit.sh
@@ -8,8 +8,7 @@ VERBOSE=1 $make_j
 # "/" intentional! (just to make cat exit with an error code)
 if [[ $MPI == 'none' ]]; then OMP_NUM_THREADS=4 make test || cat Testing/Temporary/LastTest.log /; fi
 
-# don't run Debug bconds and absorber tests on MPI as it takes too long
-# don't run var_dt either - takes too long or stalls? it takes ca. 20s on cuda-k-2
+# don't run Debug bconds, var_dt and absorber tests on MPI as it takes too long
 if [[ $MPI != 'none' ]]; then OMP_NUM_THREADS=2 make -C kahan_sum test || cat kahan_sum/Testing/Temporary/LastTest.log /; fi
 if [[ $MPI != 'none' ]]; then OMP_NUM_THREADS=2 make -C cone_bugs test || cat cone_bugs/Testing/Temporary/LastTest.log /; fi
 if [[ $MPI != 'none' ]]; then OMP_NUM_THREADS=2 make -C shallow_water test || cat shallow_water/Testing/Temporary/LastTest.log /; fi
@@ -17,12 +16,8 @@ if [[ $MPI != 'none' ]]; then make -C concurrent_1d test || cat concurrent_1d/Te
 if [[ $MPI != 'none' ]]; then OMP_NUM_THREADS=2 make -C git_revision test || cat git_revision/Testing/Temporary/LastTest.log /; fi
 
 cmake -DCMAKE_BUILD_TYPE=Release ../
-# on MPI run absorber and bconds in release mode
+# on MPI run absorber in release mode, bconds and var_dt take too long (why? they're fast on cuda-k-4)
 if [[ $MPI != 'none' ]]; then VERBOSE=1 make -C absorber; fi
-if [[ $MPI != 'none' ]]; then VERBOSE=1 make -C bconds; fi
-if [[ $MPI != 'none' ]]; then VERBOSE=1 make -C var_dt; fi
-if [[ $MPI != 'none' ]]; then make -C var_dt test || cat var_dt/Testing/Temporary/LastTest.log /; fi
-if [[ $MPI != 'none' ]]; then make -C bconds test || cat bconds/Testing/Temporary/LastTest.log /; fi
 if [[ $MPI != 'none' ]]; then make -C absorber test || cat absorber/Testing/Temporary/LastTest.log /; fi
 
 if [[ $MPI == 'none' ]]; then VERBOSE=1 $make_j; fi
