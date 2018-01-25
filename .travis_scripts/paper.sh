0ffdb7cd9daed76fcfa1ac76bd666b353104f813
Piotr Dziekan <pdziekan@igf.fuw.edu.pl>
2018-01-25 15:34:00 +0100

    disable bouss_2d on mpi travis - too slow

--- .travis_scripts/paper.sh
+++ .travis_scripts/paper.sh
@@ -17,9 +17,8 @@ VERBOSE=1 $make_j
 # "/" intentional! (just to make cat exit with an error code)
 if [[ $MPI == 'none' ]]; then OMP_NUM_THREADS=4 make test || cat Testing/Temporary/LastTest.log /; fi
 
-# some tests take too long with mpi, so we skip them, TODO: too long on travis, disable them on travis but run them by default!
+# some tests take too long with mpi, so we skip them
 if [[ $MPI != 'none' ]]; then OMP_NUM_THREADS=4 make -C 0_basic_example test || cat 0_basic_example/Testing/Temporary/LastTest.log /; fi
 if [[ $MPI != 'none' ]]; then OMP_NUM_THREADS=4 make -C 1_advscheme_opts test || cat 1_advscheme_opts/Testing/Temporary/LastTest.log /; fi
 if [[ $MPI != 'none' ]]; then OMP_NUM_THREADS=4 make -C 7_shallow_water test || cat 7_shallow_water/Testing/Temporary/LastTest.log /; fi
-if [[ $MPI != 'none' ]]; then OMP_NUM_THREADS=4 make -C 8_boussinesq_2d test || cat 8_boussinesq_2d/Testing/Temporary/LastTest.log /; fi
 cd ../../..
