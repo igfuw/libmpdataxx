19731c7d71d81a361b40bf9a08b7abcc687e64ec
Maciej Waruszewski <mwarusz@igf.fuw.edu.pl>
2019-01-07 21:00:52 +0100

    [Travis] downgrade local clang version (to avoid Boost issues)

--- .travis_scripts/setup_local_clang.sh
+++ .travis_scripts/setup_local_clang.sh
@@ -2,8 +2,8 @@
 set -ex
 mkdir local_clang
 cd local_clang
-export CLANG_VER=clang+llvm-7.0.0-x86_64-apple-darwin
-wget http://releases.llvm.org/7.0.0/$CLANG_VER.tar.xz
+export CLANG_VER=clang+llvm-6.0.0-x86_64-apple-darwin.tar.xz
+wget http://releases.llvm.org/6.0.0/$CLANG_VER.tar.xz
 tar -xf $CLANG_VER.tar.xz
 export LLVM_PATH=`pwd`/$CLANG_VER
 export CXX=$LLVM_PATH/bin/clang++
