2fa2227f2d732c425d309d4cde63bbd0ba8c45a1
Maciej Waruszewski <mwarusz@igf.fuw.edu.pl>
2019-01-07 18:45:29 +0100

    [Travis] first pass at testing with local clang on osx

---
+++ .travis_scripts/setup_local_clang.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+set -ex
+mkdir local_clang
+cd local_clang
+export CLANG_VER=clang+llvm-7.0.0-x86_64-apple-darwin
+wget http://releases.llvm.org/7.0.0/$CLANG_VER.tar.xz
+tar -xf $CLANG_VER.tar.xz
+export LLVM_PATH=`pwd`/$CLANG_VER
+export CXX=$LLVM_PATH/bin/clang++
+export LDFLAGS=$LDFLAGS" -L "$LLVM_PATH/lib" -Wl,-rpath,"$LLVM_PATH/lib
+export CPPFLAGS=$CPPFLAGS" -I"$LLVM_PATH/include" -I"$LLVM_PATH/include/c++/v1/
+cd ..
