072ce18c7022156e15f0faeda7df3d1988c30634
Piotr Dziekan <pdziekanp@gmail.com>
2019-03-15 21:09:02 +0100

    travis: convergence_vip_1d test ran on 3 threads

--- .travis_scripts/sandbox.sh
+++ .travis_scripts/sandbox.sh
@@ -10,7 +10,7 @@ VERBOSE=1 $make_j
 OMP_NUM_THREADS=4 make -C mpi_adv test || cat mpi_adv/Testing/Temporary/LastTest.log /
 OMP_NUM_THREADS=4 ctest -R tgv_2d || cat Testing/Temporary/LastTest.log /
 OMP_NUM_THREADS=4 make -C convergence_2d_3d test || cat convergence_2d_3d/Testing/Temporary/LastTest.log /
-make -C convergence_vip_1d test || cat convergence_vip_1d/Testing/Temporary/LastTest.log /
+OMP_NUM_THREADS=4 make -C convergence_vip_1d test || cat convergence_vip_1d/Testing/Temporary/LastTest.log /
 if [[ $TRAVIS_OS_NAME == 'linux' ]]; then OMP_NUM_THREADS=4 make -C convergence_spacetime test || cat convergence_spacetime/Testing/Temporary/LastTest.log /; fi
 # with mpi it takes too long, bconds_div is ran in a separate test suite for mpi
 if [[ $MPI != 'none' ]]; then OMP_NUM_THREADS=4 make -C bconds_div test || cat bconds_div/Testing/Temporary/LastTest.log /; fi
