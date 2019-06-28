cb78ce35d00e2bf46a668b12e4167d165056afe5
pdziekan <pdziekanp@gmail.com>
2019-06-28 14:37:24 +0200

    unit test: set -x for debugging

--- .travis_scripts/unit.sh
+++ .travis_scripts/unit.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-set -e
+set -ex
 cd tests/unit
 mkdir build 
 cd build
@@ -14,3 +14,4 @@ VERBOSE=1 $make_j
 # "/" intentional! (just to make cat exit with an error code)
 OMP_NUM_THREADS=4 ctest -E test_issue || cat Testing/Temporary/LastTest.log /
 cd ../../..
+set +ex
