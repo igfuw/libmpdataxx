0d17cc3c52aa747b5cebcf9ba5889ac8c4c4cee5
pdziekan <pdziekanp@gmail.com>
2019-07-02 15:28:24 +0200

    xdmf attribute with MPI: done by rank 0 + correct size

--- libmpdata++/output/hdf5_xdmf.hpp
+++ libmpdata++/output/hdf5_xdmf.hpp
@@ -85,13 +85,19 @@ namespace libmpdataxx
 
       void record_aux(const std::string &name, typename solver_t::real_t *data)
       {
-        xdmfw.add_attribute(name, this->hdf_name(), this->shape); 
+#if defined(USE_MPI)
+        if (this->mem->distmem.rank() == 0)
+#endif
+          xdmfw.add_attribute(name, this->hdf_name(), this->mem->distmem.grid_size.data()); 
         parent_t::record_aux(name, data);
       }
       
       void record_aux_dsc(const std::string &name, const typename solver_t::arr_t &arr)
       {
-        xdmfw.add_attribute(name, this->hdf_name(), this->shape); 
+#if defined(USE_MPI)
+        if (this->mem->distmem.rank() == 0)
+#endif
+          xdmfw.add_attribute(name, this->hdf_name(),  this->mem->distmem.grid_size.data()); 
         parent_t::record_aux_dsc(name, arr);
       }
 
