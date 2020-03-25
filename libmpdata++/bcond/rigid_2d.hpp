e1f49e4753823b43bbf84e94477ccfa01ff6343b
pdziekan <pdziekanp@gmail.com>
2020-03-25 14:45:01 +0100

    sgs halos for 2d rigid bcond

--- libmpdata++/bcond/rigid_2d.hpp
+++ libmpdata++/bcond/rigid_2d.hpp
@@ -57,11 +57,11 @@ namespace libmpdataxx
 
       void fill_halos_vctr_alng(arrvec_t<arr_t> &av, const rng_t &j, const bool ad = false)
       {
-	using namespace idxperm;
+        using namespace idxperm;
         // zero velocity condition
         for (int i = this->left_halo_vctr.first(), n = halo; i <= this->left_halo_vctr.last() - (ad ? 1 : 0); ++i, --n)
         {
-	  av[d](pi<d>(i, j)) = -av[d](pi<d>(this->left_edge_sclr + n - h, j));
+          av[d](pi<d>(i, j)) = -av[d](pi<d>(this->left_edge_sclr + n - h, j));
         }
       }
 
@@ -74,8 +74,36 @@ namespace libmpdataxx
       void fill_halos_flux(arrvec_t<arr_t> &av, const rng_t &j)
       {
         using namespace idxperm;
-	av[d](pi<d>(this->left_halo_vctr.last(), j)) = -av[d](pi<d>(this->left_edge_sclr + h, j));
+        av[d](pi<d>(this->left_halo_vctr.last(), j)) = -av[d](pi<d>(this->left_edge_sclr + h, j));
       }
+
+      void fill_halos_sgs_div(arr_t &a, const rng_t &j)
+      {
+        using namespace idxperm;
+        a(pi<d>(this->left_edge_sclr - h, j)) = 2 * a(pi<d>(this->left_edge_sclr + h, j)) - a(pi<d>(this->left_edge_sclr + 1 + h, j));
+      }
+
+      void fill_halos_sgs_vctr(arrvec_t<arr_t> &av, const arr_t &b, const rng_t &j, const int offset = 0)
+      {
+        // fill halos for a staggered field based on prescribed values on the edge (b array)
+        // that is 0.5 * (a(edge-h) + a(edge+h)) = b(i)
+        using namespace idxperm;
+        const auto &a = av[offset + d];
+        a(pi<d>(this->left_edge_sclr - h, j)) = 2 * b(pi<d>(this->left_edge_sclr, j)) - a(pi<d>(this->left_edge_sclr + h, j));
+      }
+
+      void fill_halos_sgs_tnsr(arrvec_t<arr_t> &av, const arr_t &u, const arr_t &div, const rng_t &j, const real_t di)
+      {
+        using namespace idxperm;
+        const auto &a = av[d];
+        a(pi<d>(this->left_edge_sclr - h, j)) = 2 * ( ( 3 * u(pi<d>(this->left_edge_sclr + 1, j))
+                                                      - 2 * u(pi<d>(this->left_edge_sclr, j))
+                                                      -     u(pi<d>(this->left_edge_sclr + 2, j))
+                                                      ) / di
+                                                    - div(pi<d>(this->left_edge_sclr - h, j))
+                                                    );
+      }
+
     };
 
     template <typename real_t, int halo, bcond_e knd, drctn_e dir, int n_dims, int d>
@@ -124,11 +152,11 @@ namespace libmpdataxx
 
       void fill_halos_vctr_alng(arrvec_t<arr_t> &av, const rng_t &j, const bool ad = false)
       {
-	using namespace idxperm;
+        using namespace idxperm;
         // zero velocity condition
         for (int i = this->rght_halo_vctr.first() + (ad ? 1 : 0), n = 1; i <= this->rght_halo_vctr.last(); ++i, ++n)
         {
-	  av[d](pi<d>(i, j)) = -av[d](pi<d>(this->rght_edge_sclr - n + h, j));
+          av[d](pi<d>(i, j)) = -av[d](pi<d>(this->rght_edge_sclr - n + h, j));
         }
       }
       
@@ -140,9 +168,36 @@ namespace libmpdataxx
       
       void fill_halos_flux(arrvec_t<arr_t> &av, const rng_t &j)
       {
-	using namespace idxperm;
+        using namespace idxperm;
         // zero flux condition
-	av[d](pi<d>(this->rght_halo_vctr.first(), j)) = -av[d](pi<d>(this->rght_edge_sclr - h, j));
+        av[d](pi<d>(this->rght_halo_vctr.first(), j)) = -av[d](pi<d>(this->rght_edge_sclr - h, j));
+      }
+
+      void fill_halos_sgs_div(arr_t &a, const rng_t &j)
+      {
+        using namespace idxperm;
+        a(pi<d>(this->rght_edge_sclr + h, j)) = 2 * a(pi<d>(this->rght_edge_sclr - h, j)) - a(pi<d>(this->rght_edge_sclr - 1 - h, j));
+      }
+
+      void fill_halos_sgs_vctr(arrvec_t<arr_t> &av, const arr_t &, const rng_t &j, const int offset = 0)
+      {
+        // fill halos for a staggered field based on prescribed values on the edge (b array)
+        // that is 0.5 * (a(edge-h) + a(edge+h)) = b(i)
+        using namespace idxperm;
+        const auto &a = av[offset + d];
+        a(pi<d>(this->rght_edge_sclr + h, j)) = 2 * b(pi<d>(this->rght_edge_sclr, j)) - a(pi<d>(this->rght_edge_sclr - h, j));
+      }
+
+      void fill_halos_sgs_tnsr(arrvec_t<arr_t> &av, const arr_t &u, const arr_t &div, const rng_t &j, const real_t di)
+      {
+        using namespace idxperm;
+        const auto &a = av[d];
+        a(pi<d>(this->rght_edge_sclr + h, j)) = 2 * ( ( 3 * u(pi<d>(this->rght_edge_sclr - 1, j))
+                                                      - 2 * u(pi<d>(this->rght_edge_sclr, j))
+                                                      -     u(pi<d>(this->rght_edge_sclr - 2, j))
+                                                      ) / di
+                                                    - div(pi<d>(this->rght_edge_sclr + h, j))
+                                                    );
       }
     };
   } // namespace bcond
