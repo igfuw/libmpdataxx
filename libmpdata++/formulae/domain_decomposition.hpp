2c532ec18d9299dcbb27e0404373dece46bc1a21
Piotr Dziekan <pdziekanp@gmail.com>
2022-09-28 11:22:20 +0200

    domain decmposition functions static

--- libmpdata++/formulae/domain_decomposition.hpp
+++ libmpdata++/formulae/domain_decomposition.hpp
@@ -15,19 +15,19 @@ namespace libmpdataxx
     namespace detail
     {
       // helper methods to define subdomain ranges
-      int min(const int &span, const int &rank, const int &size)
+      static int min(const int &span, const int &rank, const int &size)
       {
         return rank * span / size;
       }
 
-      int max(const int &span, const int &rank, const int &size)
+      static int max(const int &span, const int &rank, const int &size)
       {
         return min(span, rank + 1, size) - 1;
       }
     };
 
     // get part of 'span' assigned to 'rank' (out of 'size' ranks)
-    rng_t slab(
+    static rng_t slab(
       const rng_t &span,
       const int &rank = 0,
       const int &size = 1
