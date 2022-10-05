a7e4a9f6c99f71cf7c7966c3639d0c18e39a2412
Piotr Dziekan <pdziekanp@gmail.com>
2022-10-05 15:29:42 +0200

    formulae for averaging from refined to regular grid: working but without bcond

--- libmpdata++/formulae/refined_grid.hpp
+++ libmpdata++/formulae/refined_grid.hpp
@@ -17,13 +17,12 @@ namespace libmpdataxx
       // calculate regular grid scalar as an average from the refined grid
       // modifies the refined grid scalar!
       // 3d version
-      template<class real_t, class arr_t, class rng_t>
+      template<class real_t, class arr_t, class ijk_t>
       void spatial_average_ref2reg(
         arr_t arr,
-        const rng_t &i,  // index of the refined point that overlaps with the regular point
-        const rng_t &j,  // ditto
-        const rng_t &k,  // ditto
-        const int &dist  // average calculate from the box (i-dist, i+dist)(j-dist,j+dist)(k-dist.k+dist)
+        const ijk_t &ijk,     // index of the refined point that overlaps with the regular point
+        const int &dist,      // average calculate from the box (i-dist, i+dist)(j-dist,j+dist)(k-dist.k+dist)
+        const bool volume_avg // should averaging account weights proportional to cell volume (refined cells at box edges are only partially within the regular cell)
       )
       {
 //        using idxperm::pis;
@@ -37,19 +36,20 @@ namespace libmpdataxx
          // if(d==2)
          //   std::cerr << "range<" << d << ">: " << j << " " << k << " " << i << std::endl;
 
+        real_t norm = 0;
         for(int id=-dist; id<=dist; ++id)
           for(int jd=-dist; jd<=dist; ++jd)
             for(int kd=-dist; kd<=dist; ++kd)
             {
+              real_t w = 1; // weight
               if(id==0 && jd==0 && kd==0) continue; // dont add the overlapping point
-              real_t w = 1; // weight of the point - points on edges of the averaging box have smaller weights
               if(id==-dist || id == dist) w/=2.;
               if(jd==-dist || jd == dist) w/=2.;
               if(kd==-dist || kd == dist) w/=2.;
-              arr(i, j, k) += w * arr(i+id, j+jd, k+kd);
+              arr(ijk) += w * arr(ijk[0]+id, ijk[1]+jd, ijk[2]+kd);
+              norm += w;
             }
-        // divide by number of cells
-        arr(i, j, k) /= pow(2*dist+1, 3);
+        arr(ijk) /= norm+1; // +1 because the center cell was not added to norm
       }
     };
   };
