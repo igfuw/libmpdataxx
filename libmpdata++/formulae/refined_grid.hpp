ff8c2a4fa1e3d8a2cf06f5a1cf9570c78238e9b3
Piotr Dziekan <plgpdziekan@login01.athena.cyfronet.pl>
2023-04-05 12:34:34 +0200

    use halo in spatial_avergae_ref_2_reg; TODO: if it works, grid_size is not needed

--- libmpdata++/formulae/refined_grid.hpp
+++ libmpdata++/formulae/refined_grid.hpp
@@ -17,15 +17,14 @@ namespace libmpdataxx
       // calculate regular grid scalar as an average from the refined grid
       // modifies the refined grid scalar!
       // 3d version
-      // averaged only from points within the domain, halos outside of the modeled domain are not used
-      // however, halos between MPI domains are used;
+      // assumes refined halos are filled
       // it is assumed that these are halos are large enough!
       // currently, the halo between MPI domains is n_ref+n_ref/2
       template<class real_t, class arr_t, class ijk_t>
       void spatial_average_ref2reg(
         arr_t arr,
         const ijk_t &ijk,                    // index of the refined point that overlaps with the regular point
-        const int &dist,                     // average calculate from the box (i-dist, i+dist)(j-dist,j+dist)(k-dist.k+dist)
+        const int &dist,                     // average is calculated from the box (i-dist, i+dist)(j-dist,j+dist)(k-dist.k+dist)
         const std::array<int, 3> grid_size,  // number of cells in the domain in each direction (total, not for this MPI process), needed for averaging on edges so that it does not go out of the domain
         const bool volume_avg                // should averaging account weights proportional to cell volume (refined cells at box edges are only partially within the regular cell)
       )
@@ -42,10 +41,10 @@ namespace libmpdataxx
                   {
                     if(id==0 && jd==0 && kd==0) continue; // dont add the overlapping point
                     // dont add points outside of the domain
-                    if(i+id < 0 || i+id > grid_size[0]-1 ||
-                       j+jd < 0 || j+jd > grid_size[1]-1 ||
-                       k+kd < 0 || k+kd > grid_size[2]-1) 
-                       continue;
+                    //if(i+id < 0 || i+id > grid_size[0]-1 ||
+                    //   j+jd < 0 || j+jd > grid_size[1]-1 ||
+                    //   k+kd < 0 || k+kd > grid_size[2]-1) 
+                    //   continue;
 
                     real_t w = 1; // weight
                     if(volume_avg)
